b782b3d85f2278246506aae07f568f4a161fcabb
m0dch3n <benoitthinnes@gmail.com>
2022-12-30 08:39:46 +0100

    slidertest

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Slider from "@vueform/slider";
+import { ref } from "vue";
+import { DateTime } from "luxon";
+
+const val = ref([0, 12, 15, 17, 19, 24]);
+const format = (value: number) => {
+  const beforeDecimal = Math.floor(value);
+  const afterDecimal = value - beforeDecimal;
+  const hour = beforeDecimal;
+  const minute = Math.round(60 * afterDecimal);
+  const time = DateTime.fromObject({ hour, minute });
+  return `${time.toLocaleString(DateTime.TIME_SIMPLE)}`;
+};
+</script>
+<template>
+  <div class="p-8 pt-20">
+    <Slider v-model="val" :min="0" :max="24" :step="0.25" :format="format" />
+  </div>
+</template>
+
+<style src="@vueform/slider/themes/default.css"></style>
